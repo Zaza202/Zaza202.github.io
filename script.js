f75fdafb95be0601fab19efff2886c58839bfa2b
Zaza <mohamedaladdin02@gmail.com>
2025-02-09 19:01:33 +0200

    commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// script.js
+// Smooth scrolling for navigation links
 document.querySelectorAll('nav ul li a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
